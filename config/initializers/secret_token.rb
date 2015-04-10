c52fb071c9bf323140e138de4f43b7da3cdee9cc
wzx <wzxfate@gmail.com>
2015-04-10 16:11:09 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QRcodeOrder::Application.config.secret_key_base = 'c0bd70e6d46a799c772e763f5b6e8f566eb0db1bca25302e34ce7e16122899d8731aa5fba5e25baba2924f4dc222559c90bc081f3b49ef20474ce7f85b097320'
